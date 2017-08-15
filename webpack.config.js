69c5cacd9ec740ad99005eb92dda0e5e7732e8e8
zhangying29 <zhangying29@58ganji.com>
2017-08-16 00:11:01 +0800

    add callege

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
     main: './js/index',
     list: './js/list',
     search: './js/search',
+    college: './js/college',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -121,6 +122,16 @@ module.exports = {
       filename: 'search.html',
       template: './search.html',
       chunks: ['search'],
+      inject: ['body', 'head'],
+      /*minify: {
+       removeComments: true,
+       collapseWhitespace: true
+       }*/
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'college.html',
+      template: './college.html',
+      chunks: ['college'],
       inject: ['body', 'head'],
       /*minify: {
        removeComments: true,
